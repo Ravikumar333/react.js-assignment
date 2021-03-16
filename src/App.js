ff2ae910f085fd595601fd8465c0b47526c93ec0
Ravikumar333 <raveenthandra@gmail.com>
2021-03-16 15:41:25 +0530

    my react.app

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListItems from './ListItems'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrash)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== "") {
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item =>
+      item.key !== key);
+    this.setState({
+      items: filteredItems
+    })
+
+  }
+  setUpdate(text, key) {
+    console.log("items:" + this.state.items);
+    const items = this.state.items;
+    items.map(item => {
+      if (item.key === key) {
+        console.log(item.key + "    " + key)
+        item.text = text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="TITLE" value={this.state.currentItem.text1} onChange={this.handleInput}></input><br />
+            <input type="text" placeholder="DESCRIPTION" value={this.state.currentItem.text2} onChange={this.handleInput}></input><br />
+            <p align="center">
+              <button type="submit">SUBMIT</button>
+            </p>
+          </form>
+          <p>{this.state.items.text1}</p>
+          <p>{this.state.items.text2}</p>
+
+          <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate} />
+          <ListItems items={this.state.items} addItem={this.addItem} setUpdate={this.setUpdate} />
+
+        </header>
+      </div>
+    );
+  }
+}
+
+
+export default App;
